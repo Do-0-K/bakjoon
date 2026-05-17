dd3b8abce655fbcb6a19773ce66c9084cfb81b80
Do-0-K <135100369+Do-0-K@users.noreply.github.com>
2026-05-17 22:09:39 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    string ab = to_string(a) + to_string(b);
+    string ba = to_string(b) + to_string(a);
+
+    return max(stoi(ab), stoi(ba));
+    
+    return answer;
+}
