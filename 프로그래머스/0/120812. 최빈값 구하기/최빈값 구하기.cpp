1f108ef4a23d22eb0488560a0c39eb3094bfeb75
Do-0-K <135100369+Do-0-K@users.noreply.github.com>
2026-05-30 15:29:13 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    int arr[1001]={0};
+    int max=0;
+    int max_cnt=0;
+    
+    if(array.size()==1){
+        return answer = array[0];
+    }
+    
+    for(int i=0;i<array.size();++i){
+        arr[array[i]]++;
+    }
+    
+    for(int i=0;i<1001;++i){
+        if(max_cnt != 0 && max_cnt == arr[i])
+        {
+            answer = -1;
+        }
+        else if(arr[i] > max_cnt)
+        {
+            max = i;
+            max_cnt = arr[i];
+            answer = max;
+        }
+    }
+    return answer;
+}
