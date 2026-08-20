fd9177a7b792437238c53bdc641e3a3b329f840a
Do-0-K <135100369+Do-0-K@users.noreply.github.com>
2026-08-20 09:38:33 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.25 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,15 @@
+def solution(n):
+    d = [0] * (n+1)
+    
+    d[1] = 1
+    if n==1:
+        return 1
+    
+    d[2] = 2
+    if n==2:
+        return 2
+    
+    for i in range(3,n+1):
+        d[i] = (d[i-2] + d[i-1]) % 1234567
+        
+    return d[n]
