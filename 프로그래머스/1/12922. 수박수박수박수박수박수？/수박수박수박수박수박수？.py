416c4bef5bd441afffea4c97974ee254fa8a2dbc
Do-0-K <135100369+Do-0-K@users.noreply.github.com>
2026-09-04 14:18:29 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.47 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -6,3 +6,7 @@ def solution(n):
         else:
             answer += '박'
     return answer
+
+# 이런 방법이 있긴한데 string으로 해야 되는 문제긴 하다
+#def solution(n):
+#   return ['수' if i%2==0 else '박' for i in range(n)]
